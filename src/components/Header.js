1ec78f2503b51f5d500453e8484871625bb7a970
MLDEV101011 <88951704+MLDEV101011@users.noreply.github.com>
2022-01-04 22:24:54 -0500

    small changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "./Navigation";
 
 function Header() {
-  const [showMenu, setShowMenu] = useState(true);
 
   let hamburger = <Navigation />;
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Jumbotron from "../components/Jumbotron";
 import banner from "../images/banner.png";
 
 function Home() {
